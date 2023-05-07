a15e492a855f5851b7d733a0f98978605b6bc120
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-06 23:46:21 -0400

    refactor: Add TinyNeuralNetwork class and clean up sample
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- genesis/tnn/tnn.py
+++ genesis/tnn/tnn.py
@@ -1,80 +1,69 @@
-# Tiny Neural Network
-# genesis/tnn/tnn.py
 import numpy as np
 
-np.random.seed(0)
 
+class TinyNeuralNetwork:
+    def __init__(self, input_dim, hidden_dim, output_dim):
+        np.random.seed(0)
+        self.W1 = np.random.randn(input_dim, hidden_dim)
+        self.b1 = np.zeros((1, hidden_dim))
+        self.W2 = np.random.randn(hidden_dim, output_dim)
+        self.b2 = np.zeros((1, output_dim))
 
-# Activation function
-def sigmoid(x):
-    return 1 / (1 + np.exp(-x))
+    @staticmethod
+    def sigmoid(x):
+        return 1 / (1 + np.exp(-x))
 
+    @staticmethod
+    def sigmoid_derivative(x):
+        return x * (1 - x)
 
-# Derivative of the activation function
-def sigmoid_derivative(x):
-    return x * (1 - x)
+    def train(self, X, y, epochs=10000, learning_rate=0.1):
+        for epoch in range(epochs):
+            layer1 = self.sigmoid(np.dot(X, self.W1) + self.b1)
+            layer2 = self.sigmoid(np.dot(layer1, self.W2) + self.b2)
 
+            error = y - layer2
 
-# Define dimensions
-input_dim = 2
-hidden_dim = 2
-output_dim = 1
+            delta2 = error * self.sigmoid_derivative(layer2)
+            delta1 = np.dot(delta2, self.W2.T) * self.sigmoid_derivative(layer1)
 
-# Initialize weights and biases
-W1 = np.random.randn(input_dim, hidden_dim)
-b1 = np.zeros((1, hidden_dim))
-W2 = np.random.randn(hidden_dim, output_dim)
-b2 = np.zeros((1, output_dim))
+            self.W2 += learning_rate * np.dot(layer1.T, delta2)
+            self.b2 += learning_rate * np.sum(delta2, axis=0, keepdims=True)
+            self.W1 += learning_rate * np.dot(X.T, delta1)
+            self.b1 += learning_rate * np.sum(delta1, axis=0, keepdims=True)
+
+    def predict(self, test_input):
+        layer1 = self.sigmoid(np.dot(test_input, self.W1) + self.b1)
+        layer2 = self.sigmoid(np.dot(layer1, self.W2) + self.b2)
+        return layer2
+
+    def save_model(self, model_path):
+        np.save(model_path, [self.W1, self.b1, self.W2, self.b2])
+
+    def load_model(self, model_path):
+        self.W1, self.b1, self.W2, self.b2 = np.load(model_path, allow_pickle=True)
+
 
 # Define input data and labels
 X = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
 y = np.array([[0], [1], [1], [0]])
 
-# Training parameters
-epochs = 10000
-learning_rate = 0.1
-
-# Training loop
-for epoch in range(epochs):
-    # Forward pass
-    layer1 = sigmoid(np.dot(X, W1) + b1)
-    layer2 = sigmoid(np.dot(layer1, W2) + b2)
-
-    # Compute the error
-    error = y - layer2
-
-    # Backward pass
-    delta2 = error * sigmoid_derivative(layer2)
-    delta1 = np.dot(delta2, W2.T) * sigmoid_derivative(layer1)
-
-    # Update weights and biases
-    W2 += learning_rate * np.dot(layer1.T, delta2)
-    b2 += learning_rate * np.sum(delta2, axis=0, keepdims=True)
-    W1 += learning_rate * np.dot(X.T, delta1)
-    b1 += learning_rate * np.sum(delta1, axis=0, keepdims=True)
+# Initialize and train the network
+tnn = TinyNeuralNetwork(input_dim=2, hidden_dim=2, output_dim=1)
+tnn.train(X, y)
 
 # Test the trained model
 test_input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-layer1 = sigmoid(np.dot(test_input, W1) + b1)
-layer2 = sigmoid(np.dot(layer1, W2) + b2)
-
+output = tnn.predict(test_input)
 print("Output after training:")
-print(layer2)
+print(output)
 
-# Save model weights and biases
-np.save("model_weights.npy", [W1, b1, W2, b2])
-
-
-# Load model and perform inference
-def load_and_inference(test_input, model_path):
-    W1, b1, W2, b2 = np.load(model_path, allow_pickle=True)
-    layer1 = sigmoid(np.dot(test_input, W1) + b1)
-    layer2 = sigmoid(np.dot(layer1, W2) + b2)
-    return layer2
-
+# Save and load the model
+tnn.save_model("model_weights.npy")
+tnn.load_model("model_weights.npy")
 
 # Test the loaded model
 test_input = np.array([[0, 0], [0, 1], [1, 0], [1, 1]])
-output = load_and_inference(test_input, "model_weights.npy")
+output = tnn.predict(test_input)
 print("Output after loading the model:")
 print(output)
