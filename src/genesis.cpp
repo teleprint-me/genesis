1072de28c9d9f010e439c9c37901c57d7a1b217e
teleprint-me <77757836+teleprint-me@users.noreply.github.com>
2023-05-09 11:51:06 -0400

    fix: Format whitespacing for consistency
    Signed-off-by: teleprint-me <77757836+teleprint-me@users.noreply.github.com>

--- src/genesis.cpp
+++ src/genesis.cpp
@@ -10,38 +10,38 @@
 
 int main(void)
 {
-    if (!glfwInit())
-    {
-        fprintf(stderr, "Failed to initialize GLFW\n");
-        return -1;
-    }
+	if (!glfwInit())
+	{
+		fprintf(stderr, "Failed to initialize GLFW\n");
+		return -1;
+	}
 
-    GLFWwindow *window = glfwCreateWindow(1280, 720, "ImGui GLFW OpenGL3 example", NULL, NULL);
+	GLFWwindow *window = glfwCreateWindow(1280, 720, "ImGui GLFW OpenGL3 example", NULL, NULL);
 
-    if (window == NULL)
-    {
-        fprintf(stderr, "Failed to create a GLFW window\n");
-        glfwTerminate();
-        return -1;
-    }
-    
+	if (window == NULL)
+	{
+		fprintf(stderr, "Failed to create a GLFW window\n");
+		glfwTerminate();
+		return -1;
+	}
+
 	glfwMakeContextCurrent(window);
-    glfwSwapInterval(0); // Enable vsync
+	glfwSwapInterval(0); // Enable vsync
 
-    if (glewInit() != GLEW_OK)
-    {
-        fprintf(stderr, "Failed to initialize GLEW\n");
-        glfwTerminate();
-        return -1;
-    }
+	if (glewInit() != GLEW_OK)
+	{
+		fprintf(stderr, "Failed to initialize GLEW\n");
+		glfwTerminate();
+		return -1;
+	}
 
-    IMGUI_CHECKVERSION();
-    ImGui::CreateContext();
-    ImGuiIO &io = ImGui::GetIO();
-    ImGui::StyleColorsDark();
+	IMGUI_CHECKVERSION();
+	ImGui::CreateContext();
+	ImGuiIO &io = ImGui::GetIO();
+	ImGui::StyleColorsDark();
 
-    ImGui_ImplGlfw_InitForOpenGL(window, true);
-    ImGui_ImplOpenGL3_Init("#version 130");
+	ImGui_ImplGlfw_InitForOpenGL(window, true);
+	ImGui_ImplOpenGL3_Init("#version 130");
 
 	// Read the API key from the environment variable
 	const char *api_key = std::getenv("OPENAI_API_KEY");
@@ -75,20 +75,20 @@ int main(void)
 		ImGui::End();
 
 		ImGui::Render();
-        int display_w, display_h;
-        glfwGetFramebufferSize(window, &display_w, &display_h);
-        glViewport(0, 0, display_w, display_h);
-        glClearColor(0.1f, 0.1f, 0.1f, 1.00f);
-        glClear(GL_COLOR_BUFFER_BIT);
-        ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
-        glfwSwapBuffers(window);
+		int display_w, display_h;
+		glfwGetFramebufferSize(window, &display_w, &display_h);
+		glViewport(0, 0, display_w, display_h);
+		glClearColor(0.1f, 0.1f, 0.1f, 1.00f);
+		glClear(GL_COLOR_BUFFER_BIT);
+		ImGui_ImplOpenGL3_RenderDrawData(ImGui::GetDrawData());
+		glfwSwapBuffers(window);
 	}
 
 	ImGui_ImplOpenGL3_Shutdown();
-    ImGui_ImplGlfw_Shutdown();
-    ImGui::DestroyContext();
+	ImGui_ImplGlfw_Shutdown();
+	ImGui::DestroyContext();
 
-    glfwDestroyWindow(window);
-    glfwTerminate();
-    return 0;
+	glfwDestroyWindow(window);
+	glfwTerminate();
+	return 0;
 }
